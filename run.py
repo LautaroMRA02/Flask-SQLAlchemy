b3cb69e78d37f5751c2bd3b2ab2c5cc776a46539
Lautaro Rivadero <79938359+LautaroMRA02@users.noreply.github.com>
2022-04-17 23:49:29 -0300

    platilla base

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+posts = ['1']
+
+@app.route('/')
+def hello_world():
+    return f'{len(posts)} posts'
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return "Mostrando el post {}".format(slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return "post_form {}".format(post_id)
